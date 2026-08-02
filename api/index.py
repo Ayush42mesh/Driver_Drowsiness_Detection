dd73538b353b8e55de4b457c948bf8bb76063f1e
Ayush42mesh <ayush42mesh@gmail.com>
2026-08-02 17:57:34 +0530

    Revert Vercel deployment to frontend only and clean up unused files

--- api/index.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# Add root folder to sys.path to find config.py, model_loader.py, etc.
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from web_app import app
